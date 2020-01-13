e3fe709e3812310f5108da2d2b1e6c1bbb546303
Gumbers <gustavomarinsuppi@gmail.com>
2020-01-13 18:09:27 -0300

    adding warranty removed event bussines rule and its test

--- ProposalValidator.Domain/Models/Warranty.cs
+++ ProposalValidator.Domain/Models/Warranty.cs
@@ -4,6 +4,15 @@ namespace ProposalValidator.Domain.Models
 {
     public class Warranty
     {
+        public Warranty() { }
+
+        public Warranty(Guid id, decimal value, string province)
+        {
+            Id = id;
+            Value = value;
+            Province = province;
+        }
+
         public Guid Id { get; set; }
 
         public decimal Value { get; set; }
--- ProposalValidator.Domain/Proposals/Events/WarrantyRemovedEvent.cs
+++ ProposalValidator.Domain/Proposals/Events/WarrantyRemovedEvent.cs
@@ -1,17 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using ProposalValidator.Domain.Models;
 
 namespace ProposalValidator.Domain.Proposals.Events
 {
     public class WarrantyRemovedEvent : Event
     {
+        private const int WARRANTY_ID = 5;
+
         public WarrantyRemovedEvent(string[] data) : base(data)
         {
+            _warrantyId = Guid.Parse(data[WARRANTY_ID]);
         }
+        private readonly Guid _warrantyId;
 
         public override void Change(ref List<Proposal> proposals)
         {
-            throw new System.NotImplementedException();
+            proposals
+                .Find(proposal => proposal.Id == _proposalId)
+                .RemoveWarrantyBy(_warrantyId);
         }
     }
 }
