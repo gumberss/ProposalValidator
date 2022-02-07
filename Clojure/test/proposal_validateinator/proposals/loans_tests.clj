5c81c21104a654bf29aff7d0081a9bb63ae0efe7
gumberss <gustavomarinsuppi@gmail.com>
2022-02-06 21:31:10 -0300

    refac: tests and usi s/with-fn-validation

--- Clojure/test/proposal_validateinator/proposals/loans_tests.clj
+++ Clojure/test/proposal_validateinator/proposals/loans_tests.clj
@@ -1,35 +1,41 @@
 (ns proposal-validateinator.proposals.loans-tests
   (:require [clojure.test :refer :all]
-            [proposal-validateinator.proposals.loans :as l]))
+            [proposal-validateinator.proposals.loans :as l]
+            [schema.core :as s]))
+
+(def ^:private loan {:value 0 :monthly-installments-count 0})
 
 (deftest accepted-value?
-  (testing "Should return loan valid as valid when loan has a accepted value"
-    (are [loan result]
-      (= (l/accepted-value? loan) result)
-      {:value 10 :monthly-installments-count 1} false
-      {:value 29999.99 :monthly-installments-count 1} false
-      {:value 30000 :monthly-installments-count 1} true
-      {:value 3000000 :monthly-installments-count 1} true
-      {:value 3000000.01 :monthly-installments-count 1} false)))
+  (s/with-fn-validation
+    (testing "Should return loan valid as valid when loan has a accepted value"
+      (are [loan result]
+        (= (l/accepted-value? loan) result)
+        (assoc loan :value 10) false
+        (assoc loan :value 29999.99) false
+        (assoc loan :value 30000) true
+        (assoc loan :value 3000000) true
+        (assoc loan :value 3000000.01) false))))
 
 (deftest accepted-monthly-installments-count?
-  (testing "Should return monthly installments count as valid when loan has a accepted monthly installments count"
-    (are [loan result]
-      (= (l/accepted-monthly-installments-count? loan) result)
-      {:value 1 :monthly-installments-count 1} false
-      {:value 1 :monthly-installments-count (- (* 2 12) 1)} false
-      {:value 1 :monthly-installments-count (* 2 12)} true
-      {:value 1 :monthly-installments-count (* 15 12)} true
-      {:value 1 :monthly-installments-count (+ (* 15 12) 1)} false)))
+  (s/with-fn-validation
+    (testing "Should return monthly installments count as valid when loan has a accepted monthly installments count"
+      (are [loan result]
+        (= (l/accepted-monthly-installments-count? loan) result)
+        (assoc loan :monthly-installments-count 1) false
+        (assoc loan :monthly-installments-count (- (* 2 12) 1)) false
+        (assoc loan :monthly-installments-count (* 2 12)) true
+        (assoc loan :monthly-installments-count (* 15 12)) true
+        (assoc loan :monthly-installments-count (+ (* 15 12) 1)) false))))
 
 (deftest accepted-loan?
-  (testing "Should consider value and monthly installments count validator results"
-    (with-redefs [l/accepted-monthly-installments-count? (fn [_loan] true)
-                  l/accepted-value? (fn [_loan] true)]
-      (is (true? (l/accepted-loan? {}))))
-    (with-redefs [l/accepted-monthly-installments-count? (fn [_loan] false)
-                  l/accepted-value? (fn [_loan] true)]
-      (is (false? (l/accepted-loan? {}))))
-    (with-redefs [l/accepted-monthly-installments-count? (fn [_loan] true)
-                  l/accepted-value? (fn [_loan] false)]
-      (is (false? (l/accepted-loan? {}))))))
+  (s/with-fn-validation
+    (testing "Should consider value and monthly installments count validator results"
+      (with-redefs [l/accepted-monthly-installments-count? (fn [_loan] true)
+                    l/accepted-value? (fn [_loan] true)]
+        (is (true? (l/accepted-loan? loan))))
+      (with-redefs [l/accepted-monthly-installments-count? (fn [_loan] false)
+                    l/accepted-value? (fn [_loan] true)]
+        (is (false? (l/accepted-loan? loan))))
+      (with-redefs [l/accepted-monthly-installments-count? (fn [_loan] true)
+                    l/accepted-value? (fn [_loan] false)]
+        (is (false? (l/accepted-loan? loan)))))))
